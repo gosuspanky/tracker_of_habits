e2986abc6980276d8264c4608e6b6884ecde072e
Artur Gaetov <agaetov@gmail.com>
2024-06-28 18:54:00 +0300

    первичные настройки

---
+++ config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+from config import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("users/", include("users.urls", namespace="users")),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        verbose_name="Почта", unique=True, help_text="Укажите почту"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
+
+    def __str__(self):
+        return self.email
